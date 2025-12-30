b57cee22a1b8e7c5787226ee150d95b5d7924f84
magyx <w.magyx@gmail.com>
2025-12-30 01:07:44 +0100

    chore: add easy 1 pipeline_factory macro

--- src/lib.rs
+++ src/lib.rs
@@ -15,22 +15,33 @@ pub mod widget;
 pub mod winit;
 
 #[macro_export]
+macro_rules! pipeline_factory {
+    ( $ty:path ) => {{
+        fn __factory(
+            gpu: &$crate::graphics::Gpu,
+            surface_format: &wgpu::TextureFormat,
+            buffers: &[wgpu::VertexBufferLayout],
+            texture_bgl: &wgpu::BindGroupLayout,
+            ranges: &[wgpu::PushConstantRange],
+        ) -> ::std::boxed::Box<dyn $crate::render::pipeline::Pipeline> {
+            ::std::boxed::Box::new(<$ty>::new(
+                gpu,
+                surface_format,
+                buffers,
+                texture_bgl,
+                ranges,
+            ))
+        }
+        __factory as $crate::render::PipelineFactoryFn
+    }};
+}
+
+#[macro_export]
 macro_rules! pipeline_factories {
     ( $( $name:literal => $ty:path ),+ $(,)? ) => {{
         [
             $(
-                ($name, {
-                    fn __factory(
-                        gpu: &$crate::graphics::Gpu,
-                        surface_format: &wgpu::TextureFormat,
-                        buffers: &[wgpu::VertexBufferLayout],
-                        texture_bgl: &wgpu::BindGroupLayout,
-                        ranges: &[wgpu::PushConstantRange],
-                    ) -> ::std::boxed::Box<dyn $crate::render::pipeline::Pipeline> {
-                        ::std::boxed::Box::new(<$ty>::new(gpu, surface_format, buffers, texture_bgl, ranges))
-                    }
-                    __factory as $crate::render::PipelineFactoryFn
-                }),
+                ($name, $crate::pipeline_factory!($ty)),
             )+
         ]
     }};
