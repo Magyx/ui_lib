6815994c35936b46e2f7ffc12ddadbfd3045ce21
magyx <w.magyx@gmail.com>
2026-07-15 22:06:07 +0200

    fix: tracing is not required

--- examples/common/trace.rs
+++ examples/common/trace.rs
@@ -1,20 +1,23 @@
 pub fn init() {
-    use tracing_subscriber::util::SubscriberInitExt;
-    use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+    #[cfg(feature = "tracing")]
+    {
+        use tracing_subscriber::util::SubscriberInitExt;
+        use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
-    ui::ui_tracy_global_allocator!(0);
+        ui::ui_tracy_global_allocator!(0);
 
-    let _ = tracing_log::LogTracer::init();
+        let _ = tracing_log::LogTracer::init();
 
-    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+        let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
-    tracing_subscriber::registry()
-        .with(filter)
-        .with(fmt::layer().with_target(true))
-        .try_init()
-        .ok();
+        tracing_subscriber::registry()
+            .with(filter)
+            .with(fmt::layer().with_target(true))
+            .try_init()
+            .ok();
 
-    std::panic::set_hook(Box::new(|panic| {
-        tracing::error!(panic = ?panic, "panic");
-    }));
+        std::panic::set_hook(Box::new(|panic| {
+            tracing::error!(panic = ?panic, "panic");
+        }));
+    }
 }
