0b90c8abdfe8ddfb5eb0da239ee3939b1b66ca1a
magyx <w.magyx@gmail.com>
2026-08-14 14:42:45 +0200

    fix: teach macro to use different name

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,7 +2,31 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
-#[proc_macro_derive(Pipeline, attributes(instance_data))]
+fn get_ui_path(attrs: &[syn::Attribute]) -> syn::Result<syn::Path> {
+    let mut ui_path = None;
+
+    for attr in attrs {
+        if attr.path().is_ident("ui") {
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("crate") {
+                    let value = meta.value()?;
+                    let string_literal: syn::LitStr = value.parse()?;
+                    ui_path = Some(string_literal.parse::<syn::Path>()?);
+                    Ok(())
+                } else {
+                    Err(meta.error("unsupported ui attribute"))
+                }
+            })?;
+        }
+    }
+
+    Ok(match ui_path {
+        Some(p) => p,
+        None => syn::parse_quote!(::ui),
+    })
+}
+
+#[proc_macro_derive(Pipeline, attributes(instance_data, ui))]
 pub fn derive_pipeline(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
     let name = &input.ident;
@@ -18,10 +42,14 @@ pub fn derive_pipeline(item: TokenStream) -> TokenStream {
         .into();
     }
 
-    let mut instance_type: syn::Type = syn::parse_quote!(::ui::primitive::Primitive);
+    let ui_path = match get_ui_path(&input.attrs) {
+        Ok(path) => path,
+        Err(e) => return e.to_compile_error().into(),
+    };
+
+    let mut instance_type: syn::Type = syn::parse_quote!(#ui_path::primitive::Primitive);
     for attr in &input.attrs {
         if attr.path().is_ident("instance_data") {
-            // Note: If using syn 1.x, use `attr.path.is_ident`
             match attr.parse_args::<syn::Type>() {
                 Ok(ty) => instance_type = ty,
                 Err(err) => return err.to_compile_error().into(),
@@ -30,7 +58,7 @@ pub fn derive_pipeline(item: TokenStream) -> TokenStream {
     }
 
     quote! {
-        impl ::ui::render::pipeline::PipelineSlot for #name {
+        impl #ui_path::render::pipeline::PipelineSlot for #name {
             fn slot() -> &'static ::core::sync::atomic::AtomicU32
             where
                 Self: Sized,
@@ -44,19 +72,24 @@ pub fn derive_pipeline(item: TokenStream) -> TokenStream {
                 self
             }
         }
-        impl ::ui::primitive::Instanced<#instance_type> for #name {}
+        impl #ui_path::primitive::Instanced<#instance_type> for #name {}
     }
     .into()
 }
 
-#[proc_macro_derive(Widget)]
+#[proc_macro_derive(Widget, attributes(ui))]
 pub fn derive_widget(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
     let name = &input.ident;
 
+    let ui_path = match get_ui_path(&input.attrs) {
+        Ok(path) => path,
+        Err(e) => return e.to_compile_error().into(),
+    };
+
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
     quote! {
-        impl #impl_generics ::ui::widget::IntoElement for #name #ty_generics #where_clause {}
+        impl #impl_generics #ui_path::widget::IntoElement for #name #ty_generics #where_clause {}
     }
     .into()
 }
